c88a4681414ff617b849e50f5ae463ef6293e115
Costel Corbuleac <costel.corbuleac@luminoslabs.com>
2020-12-05 10:02:10 +0200

    Module 1

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,25 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <div>
+        <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+        <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    </div>
+    <div>
+        <label for="finishedReading">
+            Finished Reading
+        </label>
+        <input type="checkbox" name="finishedReading" id="finishedReading" v-model="bookData.finishedReading">
+    </div>
+    <div>
+        <label for="bought">
+            bought
+        </label>
+        <input type="radio" name="ownership" value="bought" id="bought" v-model="bookData.ownership">
+        <label for="borrowed">
+            borrowed
+        </label>
+        <input type="radio" name="ownership" value="borrowed" id="borrowed" v-model="bookData.ownership">
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,13 +30,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+        bookData: {
+            bookTitle: "",
+            bookAuthor: "",
+            finishedReading: false,
+            ownership: []
+        }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
